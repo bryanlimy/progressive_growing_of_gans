bc02bfed12bc141b33e4eed95e4f1eb1604adbee
Bryan Li <bryanlimy@gmail.com>
2019-01-03 13:44:13 -0500

    modify config

--- config.py
+++ config.py
@@ -9,11 +9,21 @@
 # Convenience class that behaves exactly like dict(), but allows accessing
 # the keys and values using the attribute syntax, i.e., "mydict.key = value".
 
+
 class EasyDict(dict):
-    def __init__(self, *args, **kwargs): super().__init__(*args, **kwargs)
-    def __getattr__(self, name): return self[name]
-    def __setattr__(self, name, value): self[name] = value
-    def __delattr__(self, name): del self[name]
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+  def __getattr__(self, name):
+    return self[name]
+
+  def __setattr__(self, name, value):
+    self[name] = value
+
+  def __delattr__(self, name):
+    del self[name]
+
 
 #----------------------------------------------------------------------------
 # Paths.
@@ -25,32 +35,40 @@ result_dir = 'results'
 # TensorFlow options.
 
 tf_config = EasyDict()  # TensorFlow session config, set by tfutil.init_tf().
-env = EasyDict()        # Environment variables, set by the main program in train.py.
+env = EasyDict()  # Environment variables, set by the main program in train.py.
 
-tf_config['graph_options.place_pruned_graph']   = True      # False (default) = Check that all ops are available on the designated device. True = Skip the check for ops that are not used.
+tf_config[
+    'graph_options.place_pruned_graph'] = True  # False (default) = Check that all ops are available on the designated device. True = Skip the check for ops that are not used.
 #tf_config['gpu_options.allow_growth']          = False     # False (default) = Allocate all GPU memory at the beginning. True = Allocate only as much GPU memory as needed.
 #env.CUDA_VISIBLE_DEVICES                       = '0'       # Unspecified (default) = Use all available GPUs. List of ints = CUDA device numbers to use.
-env.TF_CPP_MIN_LOG_LEVEL                        = '1'       # 0 (default) = Print all available debug info from TensorFlow. 1 = Print warnings and errors, but disable debug info.
+env.TF_CPP_MIN_LOG_LEVEL = '1'  # 0 (default) = Print all available debug info from TensorFlow. 1 = Print warnings and errors, but disable debug info.
 
 #----------------------------------------------------------------------------
 # Official training configs, targeted mainly for CelebA-HQ.
 # To run, comment/uncomment the lines as appropriate and launch train.py.
 
-desc        = 'pgan'                                        # Description string included in result subdir name.
-random_seed = 1000                                          # Global random seed.
-dataset     = EasyDict()                                    # Options for dataset.load_dataset().
-train       = EasyDict(func='train.train_progressive_gan')  # Options for main training func.
-G           = EasyDict(func='networks.G_paper')             # Options for generator network.
-D           = EasyDict(func='networks.D_paper')             # Options for discriminator network.
-G_opt       = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8) # Options for generator optimizer.
-D_opt       = EasyDict(beta1=0.0, beta2=0.99, epsilon=1e-8) # Options for discriminator optimizer.
-G_loss      = EasyDict(func='loss.G_wgan_acgan')            # Options for generator loss.
-D_loss      = EasyDict(func='loss.D_wgangp_acgan')          # Options for discriminator loss.
-sched       = EasyDict()                                    # Options for train.TrainingSchedule.
-grid        = EasyDict(size='1080p', layout='random')       # Options for train.setup_snapshot_image_grid().
+desc = 'pgan'  # Description string included in result subdir name.
+random_seed = 1000  # Global random seed.
+dataset = EasyDict()  # Options for dataset.load_dataset().
+train = EasyDict(
+    func='train.train_progressive_gan')  # Options for main training func.
+G = EasyDict(func='networks.G_paper')  # Options for generator network.
+D = EasyDict(func='networks.D_paper')  # Options for discriminator network.
+G_opt = EasyDict(
+    beta1=0.0, beta2=0.99, epsilon=1e-8)  # Options for generator optimizer.
+D_opt = EasyDict(
+    beta1=0.0, beta2=0.99, epsilon=1e-8)  # Options for discriminator optimizer.
+G_loss = EasyDict(func='loss.G_wgan_acgan')  # Options for generator loss.
+D_loss = EasyDict(func='loss.D_wgangp_acgan')  # Options for discriminator loss.
+sched = EasyDict()  # Options for train.TrainingSchedule.
+grid = EasyDict(
+    size='1080p',
+    layout='random')  # Options for train.setup_snapshot_image_grid().
 
 # Dataset (choose one).
-desc += '-celebahq';            dataset = EasyDict(tfrecord_dir='celebahq'); train.mirror_augment = True
+desc += '-celebahq'
+dataset = EasyDict(tfrecord_dir='tfrecords')
+train.mirror_augment = True
 #desc += '-celeba';              dataset = EasyDict(tfrecord_dir='celeba'); train.mirror_augment = True
 #desc += '-cifar10';             dataset = EasyDict(tfrecord_dir='cifar10')
 #desc += '-cifar100';            dataset = EasyDict(tfrecord_dir='cifar100')
@@ -97,13 +115,35 @@ desc += '-celebahq';            dataset = EasyDict(tfrecord_dir='celebahq'); tra
 
 # Config presets (choose one).
 #desc += '-preset-v1-1gpu'; num_gpus = 1; D.mbstd_group_size = 16; sched.minibatch_base = 16; sched.minibatch_dict = {256: 14, 512: 6, 1024: 3}; sched.lod_training_kimg = 800; sched.lod_transition_kimg = 800; train.total_kimg = 19000
-desc += '-preset-v2-1gpu'; num_gpus = 1; sched.minibatch_base = 4; sched.minibatch_dict = {4: 128, 8: 128, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8, 512: 4}; sched.G_lrate_dict = {1024: 0.0015}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
+desc += '-preset-v2-1gpu'
+num_gpus = 1
+sched.minibatch_base = 4
+sched.minibatch_dict = {
+    4: 128,
+    8: 128,
+    16: 128,
+    32: 64,
+    64: 32,
+    128: 16,
+    256: 8,
+    512: 4
+}
+sched.G_lrate_dict = {
+    1024: 0.0015
+}
+sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
+train.total_kimg = 12000
 #desc += '-preset-v2-2gpus'; num_gpus = 2; sched.minibatch_base = 8; sched.minibatch_dict = {4: 256, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8}; sched.G_lrate_dict = {512: 0.0015, 1024: 0.002}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
 #desc += '-preset-v2-4gpus'; num_gpus = 4; sched.minibatch_base = 16; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16}; sched.G_lrate_dict = {256: 0.0015, 512: 0.002, 1024: 0.003}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
 #desc += '-preset-v2-8gpus'; num_gpus = 8; sched.minibatch_base = 32; sched.minibatch_dict = {4: 512, 8: 256, 16: 128, 32: 64, 64: 32}; sched.G_lrate_dict = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}; sched.D_lrate_dict = EasyDict(sched.G_lrate_dict); train.total_kimg = 12000
 
 # Numerical precision (choose one).
-desc += '-fp32'; sched.max_minibatch_per_gpu = {256: 16, 512: 8, 1024: 4}
+desc += '-fp32'
+sched.max_minibatch_per_gpu = {
+    256: 16,
+    512: 8,
+    1024: 4
+}
 #desc += '-fp16'; G.dtype = 'float16'; D.dtype = 'float16'; G.pixelnorm_epsilon=1e-4; G_opt.use_loss_scaling = True; D_opt.use_loss_scaling = True; sched.max_minibatch_per_gpu = {512: 16, 1024: 8}
 
 # Disable individual features.
